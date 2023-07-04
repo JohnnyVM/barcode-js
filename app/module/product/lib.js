e61ab44d8869b14f4bff674805075588cd4e2dfe
OVH Server <vmjuan90@gmail.com>
2023-07-04 19:41:28 +0000

    update handle 404

--- app/module/product/lib.js
+++ app/module/product/lib.js
@@ -1,12 +1,22 @@
 import { config }  from '../../config.js';
 
 async function fetchProduct(bar) {
-	const response = await fetch(new URL('/product/' + bar, config['url']));
-	const jsonData = await response.json();
-	if(!jsonData.length) {
-		return null;
+	try {
+		const response = await fetch(new URL('/product/' + bar, config['url']));
+		if (response.status === 404) {
+			return null;
+		}
+		if (!response.ok) {
+			throw new Error("Network response was not OK");
+		}
+		const jsonData = await response.json();
+		if(!jsonData.length) {
+			return null;
+		}
+		return jsonData;
+	} catch (error) {
+		console.error("There has been a problem with your fetch operation: ", error);
 	}
-	return jsonData;
 }
 
 export { fetchProduct };
